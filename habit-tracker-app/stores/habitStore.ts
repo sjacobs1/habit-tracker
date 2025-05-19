a40ccd573fe2bc295c643443bf11fefb0075aaa4
sebastianj7 <sebastianjacobs2000@gmail.com>
2025-05-19 11:58:17 +0200

    feat: :sparkles: local storage
    added storage for habits, persist between sessions and don't only live in state

--- habit-tracker-app/stores/habitStore.ts
+++ habit-tracker-app/stores/habitStore.ts
@@ -1,19 +1,46 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Habit } from "../models/habitModel";
 
 interface HabitStore {
   habits: Habit[];
   addHabit: (habit: Habit) => void;
-  //   removeHabit: (id: string) => void;
-  //   updateHabit: (id: string, updatedHabit: Partial<Habit>) => void;
+  deleteHabit: (id: string) => void;
+  updateHabit: (id: string, updatedHabit: Partial<Habit>) => void;
 }
 
-export const useHabitStore = create<HabitStore>((set, get) => ({
-  habits: [],
-  addHabit: (habit) => set((state) => ({ habits: [...state.habits, habit] })),
-  // removeHabit: (id) => set((state) => ({ habits: state.habits.filter(habit => habit.id !== id) })),
-  // updateHabit: (id, updatedHabit) => set((state) => ({
-  //   habits: state.habits.map((habit) =>
-  //     habit.id === id ? { ...habit, ...updatedHabit } : habit
-  //   ),
-}));
+export const useHabitStore = create<HabitStore>()(
+  persist(
+    (set, get) => ({
+      habits: [],
+      addHabit: (habit) =>
+        set((state) => ({ habits: [...state.habits, habit] })),
+      deleteHabit: (id) =>
+        set((state) => ({
+          habits: state.habits.filter((habit) => habit.id !== id),
+        })),
+      updateHabit: (id, updatedHabit) =>
+        set((state) => ({
+          habits: state.habits.map((habit) =>
+            habit.id === id ? { ...habit, ...updatedHabit } : habit
+          ),
+        })),
+    }),
+    {
+      name: "habit-storage",
+      storage: {
+        getItem: async (name: string) => {
+          const value = await AsyncStorage.getItem(name);
+          return value ? JSON.parse(value) : null;
+        },
+        setItem: async (name: string, value: any) => {
+          await AsyncStorage.setItem(name, JSON.stringify(value));
+        },
+        removeItem: async (name: string) => {
+          await AsyncStorage.removeItem(name);
+        },
+      },
+    }
+  )
+);
