4ce8f616893ab0369d7cb836053f0d0656acdeea
sebastianj7 <sebastianjacobs2000@gmail.com>
2025-06-03 15:34:35 +0200

    refactor: :recycle: change time selector
    refactored time selector on add habit to max out at 3 times, times captured on change instead of on add

--- habit-tracker-app/components/TimeSelector.tsx
+++ habit-tracker-app/components/TimeSelector.tsx
@@ -8,18 +8,11 @@ interface TimeSelectorProps {
 
 const TimeSelector = ({ addTime }: TimeSelectorProps) => {
   const [selectedTime, setSelectedTime] = useState<Date | null>(null);
-
   const handleTimeChange = (event: any, date?: Date) => {
     if (date) {
       setSelectedTime(date);
-    }
-  };
-
-  const handleAddTime = () => {
-    if (selectedTime) {
-      const timeString = selectedTime.toTimeString().slice(0, 5);
+      const timeString = date.toTimeString().slice(0, 5);
       addTime(timeString);
-      setSelectedTime(null);
     }
   };
 
@@ -31,7 +24,6 @@ const TimeSelector = ({ addTime }: TimeSelectorProps) => {
         display="default"
         onChange={handleTimeChange}
       />
-      <Button title="Add Time" onPress={handleAddTime} />
     </View>
   );
 };
